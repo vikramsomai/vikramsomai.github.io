2cf628fc2ac482d422c6c27839c8c1b3e36ad39a
Vikram somai <50650259+Vikramsomai@users.noreply.github.com>
2023-08-22 17:00:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var cursor = document.querySelector('.blob');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
