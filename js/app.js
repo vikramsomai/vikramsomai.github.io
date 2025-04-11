474ff5456e5938224e55d64c421fd6bffb27f688
vikram somai <vikramsomai4@gmail.com>
2025-04-11 11:49:24 +0530

    add mobile nav

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-var cursor = document.querySelector('.blob');
+// var cursor = document.querySelector(".blob");
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+// document.addEventListener("mousemove", function (e) {
+//   var x = e.clientX;
+//   var y = e.clientY;
+//   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+// });
+
+let menu = document.getElementById("menu");
+
+let navlinks = document.getElementsByClassName("nav-links")[0];
+
+menu.addEventListener("click", () => {
+  navlinks.classList.add("show-menu");
+  // document.getElementById("menu-container").style.display = "block";
+});
+let close = document.getElementById("close");
+close.addEventListener("click", () => {
+  // document.getElementById("menu-container").style.display = "none";
+  navlinks.classList.remove("show-menu");
 });
